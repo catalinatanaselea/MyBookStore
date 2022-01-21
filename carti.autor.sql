95251ed729ec74ac856e96d7f2fdba7ff3c1b1cf
catalinatanaselea <98126108+catalinatanaselea@users.noreply.github.com>
2022-01-21 02:07:24 +0200

    Update carti.autor.sql

--- carti.autor.sql
+++ carti.autor.sql
@@ -3,5 +3,5 @@ go
 drop table if exists carti.autor;
 go
 create table carti.autor
-		(id_autor smallint identity(1,1) primary key not null,
+		(id smallint identity(1,1) primary key not null,
 		 nume_autor varchar(50) not null)
