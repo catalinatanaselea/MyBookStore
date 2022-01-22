50066de735980134111038d0a79eb1088b8c1255
catalinatanaselea <98126108+catalinatanaselea@users.noreply.github.com>
2022-01-22 16:26:26 +0200

    Update adresa.tara.sql

--- adresa.tara.sql
+++ adresa.tara.sql
@@ -1,7 +1,7 @@
 drop table if exists adresa.tara;
 go
 create table adresa.tara
-	(id tinyint identity(1,1) primary key not null,
+	(id tinyint bit primary key not null,
 	 tara varchar(15) not null);
 go
 insert into adresa.tara(tara) 
