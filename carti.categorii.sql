2dc4e7edac0540894687ac7c9a2b556d41767794
catalinatanaselea <98126108+catalinatanaselea@users.noreply.github.com>
2022-01-22 18:28:58 +0200

    Add files via upload

---
+++ carti.categorii.sql
@@ -0,0 +1,33 @@
+use PersonalDB;
+go
+drop table if exists carti.categorii;
+go
+create table carti.categorii(
+		id_categ smallint identity(1,1) primary key not null,
+		nume_categ varchar(50) not null);
+go
+insert into carti.categorii(nume_categ) 
+	values 	('Culinare'),
+			('Arta, arhitectura'),
+			('Bibliografii, memorii, jurnale'),
+			('Lingvistiva, dictionare'),
+			('Poezie, teatru'),
+			('Fictiune'),
+			('Istorie'),
+			('Religie'),
+			('Filosofie'),
+			('Psihologie'),
+			('Stiinte sociale'),
+			('Marketing si comunicare'),
+			('Bussiness si economie'),
+			('Drept'),
+			('Medicina'),
+			('Stiinte exacte, matematici'),
+			('Natura si mediu'),
+			('Tehnica si tehnologie'),
+			('Computere si internet'),
+			('Sanatate, dezvoltare personala'),
+			('Lifestyle, sport, timp liber');
+go
+select * from carti.categorii;
+go
