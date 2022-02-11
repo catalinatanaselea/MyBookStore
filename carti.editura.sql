7ce7a9a9ec4de1afed3b5cdd4b702543d81f2c60
Catalina <98126108+catalinatanaselea@users.noreply.github.com>
2022-02-11 15:02:09 +0200

    Delete carti.editura.sql

--- carti.editura.sql
+++
@@ -1,8 +0,0 @@
-use PersonalDB;
-go
-drop table if exists carti.editura;
-go
-create table carti.editura
-		(id smallint identity(1,1) primary key not null,
-		 nume_editura varchar(30) not null);
-go
