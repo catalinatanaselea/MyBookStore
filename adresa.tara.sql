bcb02327de074af244ca3bda1c461e71b299cc89
Catalina <98126108+catalinatanaselea@users.noreply.github.com>
2022-02-11 15:01:06 +0200

    Delete adresa.tara.sql

--- adresa.tara.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists adresa.tara;
-go
-create table adresa.tara
-	(id bit primary key not null,
-	 tara varchar(15) not null);
-go
-insert into adresa.tara(tara) 
-	values ('Romania');
